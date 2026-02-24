244f14251e8b9c7d8e092bb8ac78402ea4ea99e0
Wilmar28 <wrondonbarrero@gmail.com>
2026-02-23 23:47:46 -0500

    xd

---
+++ ejemplo-react/src/pages/HomePage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Hero from '../components/Hero';
+import HomeCards from '../components/HomeCard';
+import JobListings from '../components/JobListings';
+import ViewAllJobs from '../components/ViewAllJobs';
+
+const HomePage = () => {
+  return (
+     <>
+      <Hero />
+      <HomeCards />
+      <JobListings isHome={true} />
+      <ViewAllJobs />
+    </>
+  )
+}
+
+export default HomePage
